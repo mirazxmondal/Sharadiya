e3b70c4dec8ae633ae12cdf91944557497847464
TuhinBar <tuhinbr99@gmail.com>
2022-09-18 23:39:29 +0530

    Added carousel

--- Assets/scripts/main.js
+++ Assets/scripts/main.js
@@ -1,1 +1,20 @@
 console.log("starting main js");
+
+
+//  Dhak sound
+
+const dhak = new Audio("Assets/audio/dhak.mp3");
+
+const explore = document.querySelector(".carousel");
+
+explore.addEventListener("mouseenter", () => {
+    dhak.volume = 0.1;
+    dhak.play();
+    console.log("clicked");
+});
+
+explore.addEventListener("mouseleave", () => {
+
+    dhak.pause();
+    console.log("clicked");
+});
