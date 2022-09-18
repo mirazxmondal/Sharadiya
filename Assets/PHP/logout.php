3a4cbe2e35812d642c85dcc2199695a416acc703
miraz-mondal <mirazmondal.official@gmail.com>
2022-09-19 04:01:28 +0530

    Minor mistake rectified

--- Assets/PHP/logout.php
+++ Assets/PHP/logout.php
@@ -1,6 +1,6 @@
 <?php
 // to start the session in logout page I connected with my database
-require 'connect1.php';
+require 'connect-session.php';
 session_destroy();
 header("location:index.html?msg=loggedout");
 ?>
