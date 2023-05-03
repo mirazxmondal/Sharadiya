d14e130cf77217dd0d8475636e60487d7f1c77d5
miraz-mondal <mirazmondal.official@gmail.com>
2023-05-04 03:32:39 +0530

    CSS formatted

--- Assets/PHP/admin-DB-connection.php
+++ Assets/PHP/admin-DB-connection.php
@@ -1,3 +1,7 @@
+/* This code is establishing a connection to a MySQL database named "sharadiya" on the local server
+using the username "root" and an empty password. The `mysqli_connect()` function returns a
+connection object that is stored in the `` variable. If the connection fails, the `die()`
+function is called with an error message returned by `mysqli_error()`. */
 <?php
 $conn=mysqli_connect('localhost','root','','sharadiya') or die (mysqli_error($conn));
 
