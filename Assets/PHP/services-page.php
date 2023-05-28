96365a8fee31740714ffdbd47a5b6b441a326c92
miraz-mondal <mirazmondal.official@gmail.com>
2023-05-29 04:45:25 +0530

    services-page.php favicon bug fixed

--- Assets/PHP/services-page.php
+++ Assets/PHP/services-page.php
@@ -8,7 +8,7 @@
   <title>Sharadiya</title>
   <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
-  <link rel="shortcut icon" href="../Sharadiya/Assets/images/favicon.png" type="image/x-icon" sizes="16x16 32x32" />
+  <link rel="shortcut icon" href="../images/./favicon.png" type="image/x-icon" sizes="16x16 32x32">
 </head>
 
 <body id="body">
